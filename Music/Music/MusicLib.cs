b93ba9c7a0a9d1c86548bbe65a11c5f743527c79
SeethalakshmiPerumal <blumingsee@mail.com>
2019-05-29 20:25:57 -0700

    Delete MusicLib.cs

--- Music/Music/MusicLib.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Music
-{
-    internal class MusicLib
-    {
-        public string FileName { get; internal set; }
-        public string Artist { get; set; }
-        public string Album { get; set; }
-    }
-}
